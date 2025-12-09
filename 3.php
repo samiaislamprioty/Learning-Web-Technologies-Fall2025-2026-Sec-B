dc38bd5c4044307c88f88eb747ed0c637b014783
Samiaislampriotygit config --global user.email samiaislamprioty@gmail.com <you@example.com>
2025-12-09 14:09:45 +0600

    php lab task

---
+++ 3.php
@@ -0,0 +1,20 @@
+<?php
+
+$oddNumber = 9;   
+$evenNumber = 12; 
+
+echo "Given Odd Number: $oddNumber <br>";
+if ($oddNumber % 2 == 0) {
+    echo "$oddNumber is Even <br><br>";
+} else {
+    echo "$oddNumber is Odd <br><br>";
+}
+
+echo "Given Even Number: $evenNumber <br>";
+if ($evenNumber % 2 == 0) {
+    echo "$evenNumber is Even <br>";
+} else {
+    echo "$evenNumber is Odd <br>";
+}
+
+?>
