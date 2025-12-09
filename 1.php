bd935cc92a2f5560a87290e5746dcd767e8f5180
Samiaislampriotygit config --global user.email samiaislamprioty@gmail.com <you@example.com>
2025-12-09 14:08:07 +0600

    PHP lab task

---
+++ 1.php
@@ -0,0 +1,15 @@
+<?php
+
+
+$length = 10;
+$width = 5;
+
+$area = $length * $width;
+$perimeter = 2 * ($length + $width);
+
+echo "Length: $length <br>";
+echo "Width: $width <br>";
+echo "Area of Rectangle = " . $area . "<br>";
+echo "Perimeter of Rectangle = " . $perimeter . "<br>";
+
+?>
