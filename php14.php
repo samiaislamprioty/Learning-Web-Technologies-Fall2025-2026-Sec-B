678d09db8d83c46e8c7af033f85c35e40af574fc
Samiaislampriotygit config --global user.email samiaislamprioty@gmail.com <you@example.com>
2026-01-03 22:48:23 +0600

    13

---
+++ php14.php
@@ -0,0 +1,5 @@
+<?php
+$text = "PHP";
+echo strlen($text);
+?>
+<?php
---
+++ php15.php
@@ -0,0 +1,10 @@
+<?php
+$email = "admin@gmail.com";
+$pass = "1234";
+
+if($email == "admin@gmail.com" && $pass == "1234"){
+    echo "Login Successful";
+}else{
+    echo "Login Failed";
+}
+?>
